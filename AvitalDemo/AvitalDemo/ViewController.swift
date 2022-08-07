5032a944b5aa64c4b873c3a17e0431a906b2bdbf
Jash <72010651+jash3010@users.noreply.github.com>
2022-08-07 10:31:47 +0530

    Project Created

---
+++ AvitalDemo/AvitalDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitalDemo
+//
+//  Created by MAC  on 07/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
